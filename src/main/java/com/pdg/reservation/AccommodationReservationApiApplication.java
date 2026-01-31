44fd4b4518f64005696ec33976fd80807488f122
박대광 <iieorhkd@gmail.com>
2026-01-31 13:13:14 +0900

    Initial commit

---
+++ src/main/java/com/pdg/reservation/AccommodationReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.pdg.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodationReservationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdg/reservation/AccommodationReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdg.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
